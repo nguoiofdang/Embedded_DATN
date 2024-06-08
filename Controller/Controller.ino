eb7cb91b20c04516d6cd8f855ab11555263ebcb2
DESKTOP\NguyenGiaHuy <huy30062001pn@gmail.com>
2024-06-08 16:54:22 +0700

    8/6

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,3 +1,165 @@
+// #include <Arduino.h>
+// #include <ESP8266WiFi.h>
+// #include <espnow.h>
+// #include <SD.h>
+// #include <Arduino_JSON.h>
+// #include <ArduinoJson.h>
+// #include <Firebase_ESP_Client.h>
+// #include "addons/TokenHelper.h"
+// #include "addons/RTDBHelper.h"
+
+// // Thong tin cua wifi muon truy cap
+// #define WIFI_SSID "Tang 2"
+// #define WIFI_PASSWORD "12345678"
+
+// // Firebase project API Key
+// #define API_KEY "AIzaSyDjeq8UaaphaMD_RUNITJW4XoNXIKgdoEs"
+
+// // RTDB URLefine the DTSB URL
+// #define DATABASE_URL "https://remote-gh-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+// FirebaseData fbdo;
+// FirebaseAuth auth;
+// FirebaseConfig config;
+
+// long timeSend = 0;
+// bool signupOk = false;
+// bool received = false;
+// String _confirm = "confirm";
+
+// typedef struct objectControl
+// {
+//   String address;
+//   bool control;
+// } objectControl;
+
+// objectControl objControl;
+
+// void arrayToHex(uint8_t *mac_addr, String *stringID)
+// {
+//   char hexID[18];
+
+//   sprintf(hexID, "%02X:%02X:%02X:%02X:%02X:%02X",
+//           mac_addr[0],mac_addr[1],mac_addr[2],
+//           mac_addr[3],mac_addr[4],mac_addr[5]);
+//   *stringID = String(hexID);
+// }
+
+// void hexToArray(uint8_t *mac_addr, String hexID)
+// {
+//   Serial.printf("Send fail %c \n", hexID);
+//   sscanf(hexID.c_str(), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+//           mac_addr[0],mac_addr[1],mac_addr[2],
+//           mac_addr[3],mac_addr[4],mac_addr[5]);
+// }
+
+// String feedback(String address, String node)
+// {
+//   return "Remote/" + address + "/" + node;
+// }
+
+// void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
+// {
+
+//   String address;
+//   arrayToHex(mac_addr, &address);
+
+//   if (sendStatus == 0)
+//   {
+//     Serial.printf("\nSend success");
+//     String path = feedback(objControl.address, _confirm);
+//     Firebase.RTDB.setBool(&fbdo, path, objControl.control);
+//   } 
+//   else 
+//   {
+//     Serial.printf("Send fail\n");
+//   }
+
+// }
+
+// void streamCallback(FirebaseStream data)
+// {
+
+//   // const size_t capacity = JSON_OBJECT_SIZE(2) + 30;
+
+//   DynamicJsonDocument doc(100);
+//   deserializeJson(doc, data.jsonString());
+//   if (doc["address"] == "")
+//   {
+//     return;
+//   }
+
+//   objControl.control = doc["control"];
+//   objControl.address = String(doc["address"]);
+
+//   Serial.println("Hahhaha");
+
+//   // received = true;
+
+// }
+
+// void streamTimeoutCallback(bool timeout) {
+
+// }
+
+// void setup() {
+//   Serial.begin(115200);
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+//   while(WiFi.status() != WL_CONNECTED) {
+//     Serial.print(".");
+//     delay(300);
+//   }
+
+//   config.api_key = API_KEY;
+//   config.database_url = DATABASE_URL;
+//   if (Firebase.signUp(&config, &auth, "", ""))
+//   {
+//     Serial.println("Firebase ok");
+//     signupOk = true;
+//   }
+
+//   config.token_status_callback = tokenStatusCallback;
+//   Firebase.begin(&config, &auth);
+//   Firebase.reconnectWiFi(true);
+//   Firebase.RTDB.beginStream(&fbdo, "/Control");
+//   Firebase.RTDB.setStreamCallback(&fbdo, streamCallback, streamTimeoutCallback);
+//   if (esp_now_init() != 0) {
+//     Serial.println("Error initializing ESP-NOW");
+//     return;
+//   }
+//   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+//   esp_now_register_send_cb(OnDataSent);
+// }
+
+// void loop() {
+//   if (received)
+//   {
+//     Serial.println("123");
+//     received = false;
+//     uint8_t *address;
+//     // hexToArray(address, objControl.address);
+//     if (!esp_now_is_peer_exist(address))
+//     {
+//       esp_now_add_peer(address, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+//       timeSend = millis();
+//       while (!esp_now_is_peer_exist(address))
+//       {
+//         if (millis() - timeSend >= 5000)
+//         {
+//           break;
+//         }
+//       }
+//       esp_now_send(address, (uint8_t *)&objControl, sizeof(objControl));
+//     }
+//     else
+//     {
+//       esp_now_send(address, (uint8_t *)&objControl, sizeof(objControl));
+//     }
+//   }
+//   delay(10);
+// }
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <espnow.h>
@@ -8,14 +170,14 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-// Thong tin cua wifi muon truy cap
+// Thông tin của WiFi muốn truy cập
 #define WIFI_SSID "Tang 2"
 #define WIFI_PASSWORD "12345678"
 
 // Firebase project API Key
 #define API_KEY "AIzaSyDjeq8UaaphaMD_RUNITJW4XoNXIKgdoEs"
 
-// RTDB URLefine the DTSB URL
+// RTDB URL
 #define DATABASE_URL "https://remote-gh-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
 FirebaseData fbdo;
@@ -24,7 +186,7 @@ FirebaseConfig config;
 
 long timeSend = 0;
 bool signupOk = false;
-bool control = false;
+bool received = false;
 String _confirm = "confirm";
 
 typedef struct objectControl
@@ -40,16 +202,17 @@ void arrayToHex(uint8_t *mac_addr, String *stringID)
   char hexID[18];
 
   sprintf(hexID, "%02X:%02X:%02X:%02X:%02X:%02X",
-          mac_addr[0],mac_addr[1],mac_addr[2],
-          mac_addr[3],mac_addr[4],mac_addr[5]);
+          mac_addr[0], mac_addr[1], mac_addr[2],
+          mac_addr[3], mac_addr[4], mac_addr[5]);
   *stringID = String(hexID);
 }
 
 void hexToArray(uint8_t *mac_addr, String hexID)
 {
+  Serial.printf("Send fail %s \n", hexID.c_str());
   sscanf(hexID.c_str(), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
-          mac_addr[0],mac_addr[1],mac_addr[2],
-          mac_addr[3],mac_addr[4],mac_addr[5]);
+         &mac_addr[0], &mac_addr[1], &mac_addr[2],
+         &mac_addr[3], &mac_addr[4], &mac_addr[5]);
 }
 
 String feedback(String address, String node)
@@ -59,7 +222,6 @@ String feedback(String address, String node)
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
 {
-
   String address;
   arrayToHex(mac_addr, &address);
 
@@ -68,21 +230,17 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
     Serial.printf("\nSend success");
     String path = feedback(objControl.address, _confirm);
     Firebase.RTDB.setBool(&fbdo, path, objControl.control);
-  } 
-  else 
+  }
+  else
   {
     Serial.printf("Send fail\n");
   }
-
 }
 
 void streamCallback(FirebaseStream data)
 {
-
-  // const size_t capacity = JSON_OBJECT_SIZE(2) + 30;
   DynamicJsonDocument doc(100);
   deserializeJson(doc, data.jsonString());
-
   if (doc["address"] == "")
   {
     return;
@@ -91,20 +249,27 @@ void streamCallback(FirebaseStream data)
   objControl.control = doc["control"];
   objControl.address = String(doc["address"]);
 
-  control = true;
+  Serial.println("Received data from Firebase");
 
+  received = true;
 }
 
-void streamTimeoutCallback(bool timeout) {
-
+void streamTimeoutCallback(bool timeout)
+{
+  if (timeout)
+  {
+    Serial.println("Stream timeout, resuming...");
+  }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(300);
   }
@@ -113,7 +278,7 @@ void setup() {
   config.database_url = DATABASE_URL;
   if (Firebase.signUp(&config, &auth, "", ""))
   {
-    Serial.println("Firebase ok");
+    Serial.println("Firebase OK");
     signupOk = true;
   }
 
@@ -122,7 +287,8 @@ void setup() {
   Firebase.reconnectWiFi(true);
   Firebase.RTDB.beginStream(&fbdo, "/Control");
   Firebase.RTDB.setStreamCallback(&fbdo, streamCallback, streamTimeoutCallback);
-  if (esp_now_init() != 0) {
+  if (esp_now_init() != 0)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
@@ -130,11 +296,13 @@ void setup() {
   esp_now_register_send_cb(OnDataSent);
 }
 
-void loop() {
-  if (control)
+void loop()
+{
+  if (received)
   {
-    control = false;
-    uint8_t *address;
+    Serial.println("Da nghe");
+    received = false;
+    uint8_t address[6]; // Cấp phát bộ nhớ cho địa chỉ MAC
     hexToArray(address, objControl.address);
     if (!esp_now_is_peer_exist(address))
     {
